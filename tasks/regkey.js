ecc1aa2d1bbf2f2168cb59c3664f3528f434ef8c
SergeyArtemyev <56877358+SergeyArtemyev@users.noreply.github.com>
2021-07-02 13:23:11 +0300

    Update regkey.js
    add default values

--- tasks/regkey.js
+++ tasks/regkey.js
@@ -4,13 +4,10 @@ const url = 'https://predproc-test.gbsystems.pro/pay/genkey';
 const data = `<?xml version="1.0"?>
     <root>
     <request type="genkey">
-    <login>5439</login>
-    <password>62ead7808c41cee35546305bb38e2a91</password>
+    <login>логин</login>
+    <password>пароль</password>
     <pubkey>-----BEGIN RSA PUBLIC KEY-----
-    MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCp0NFIUKFJ2LOhUY/hltJosasw
-    b8o/EUuChntW9/NGuo0+2pz8pRd0xueEX7LxQQP8VUgqX5Dxh5NuWMmBnQYg7Y6E
-    MG/v5M50TDYukFQZ55IXrqL5p/qLnzdasrxyFZ/IDIw1beDr931Ih6IfAJNu4+w1
-    FgOdcsliZK4mYb4bFwIDAQAB
+    ключ
     -----END RSA PUBLIC KEY-----
     </pubkey>
     </request>
