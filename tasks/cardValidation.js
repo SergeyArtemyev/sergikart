c12e4fc03b6875c84439abeb9e6471adb76c71ce
SergeyArtemyev <56877358+SergeyArtemyev@users.noreply.github.com>
2021-07-06 13:43:20 +0300

    fix

--- tasks/cardValidation.js
+++ tasks/cardValidation.js
@@ -2,13 +2,13 @@ const cardValidation = (request) => {
     const currentDate = new Date().getTime();
     const cardExpirationDate = new Date(`${request.expiration_year},${request.expiration_month}, 01`).getTime();
 
-    return cardExpirationDate <= currentDate   
+    return cardExpirationDate >= currentDate   
 };
 
 // implementation before getting customer from DB
 
 const isCardValid = cardValidation(request);
-if (isCardValid) {
+if (!isCardValid) {
     await putCustomMetrics({ mn, obj: { code: 400084, pid: request.project, gid: 0, isi: true, am: '0' } });
     return responseMessage(401, 400084, 'Credit card is expired');
 }
